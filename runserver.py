0c0002411421d90d91399660a5ebca00b2347b65
sokol <sokolov16082005@gmail.com>
2025-06-15 14:56:16 +0300

    Добавил папку для скачивания

--- runserver.py
+++ runserver.py
@@ -1,9 +1,11 @@
 ﻿"""
 This script runs the task_managment_system application using a development server.
 """
-
+import os
 from os import environ
 from task_managment_system import app
+
+app.config['UPLOAD_FOLDER'] = os.path.join(os.path.dirname(__file__), 'static/uploads')
 
 if __name__ == '__main__':
    
